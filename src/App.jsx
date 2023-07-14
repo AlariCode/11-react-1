f5b4e8e364721cd189d1224abe2122c6cbcb12bb
AlariCode <antonlarichev@gmail.com>
2023-07-14 16:13:13 +0300

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
 
 function App() {
 
+	// return React.createElement('div', {}, React.createElement);
+
   return (
-    <div>Проект</div>
+    <div>
+		<h1>Заголовок</h1>
+		<p>Какой-то текст</p>
+	</div>
   );
 }
 
