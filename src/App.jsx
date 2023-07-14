5ff4bcda09369c121e4ae8459bafe9dae148288d
AlariCode <antonlarichev@gmail.com>
2023-07-14 16:25:16 +0300

    React Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import './App.css';
 
 function App() {
 
-	// return React.createElement('div', {}, React.createElement);
-
   return (
-    <div>
+    <>
 		<h1>Заголовок</h1>
 		<p>Какой-то текст</p>
-	</div>
+	</>
   );
 }
 
