2999269a9cc4123ff8ab2770ed1f7486da1e67d8
AlariCode <antonlarichev@gmail.com>
2023-07-19 12:12:34 +0300

    Обработка событий

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,12 @@
 import './Button.css';
 
 function Button() {
+	const clicked = () => {
+		console.log('Привет!');
+	};
 
 	return (
-		<button className='button accent'>Сохранить</button>
+		<button onClick={clicked} className='button accent'>Сохранить</button>
 	);
 }
 
