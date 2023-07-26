6670a38969f30c439bb2b80554cc1ab21de59a01
AlariCode <antonlarichev@gmail.com>
2023-07-26 18:53:20 +0300

    Динамические классы

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -40,10 +40,10 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' style={{ border: formValidState.title ? undefined : '1px solid red' }}/>
-			<input type='date' name='date' style={{ border: formValidState.date ? undefined : '1px solid red' }} />
+			<input type='text' name='title' className={`input ${formValidState.title ? '' : 'invalid'}`}/>
+			<input type='date' name='date' className={`input ${formValidState.date ? '' : 'invalid'}`} />
 			<input type='text' name='tag' />
-			<textarea name="post" id="" cols="30" rows="10" style={{ border: formValidState.post ? undefined : '1px solid red' }}></textarea>
+			<textarea name="post" id="" cols="30" rows="10" className={`input ${formValidState.post ? '' : 'invalid'}`}></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
