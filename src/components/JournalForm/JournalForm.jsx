fcd8d8b17985c36c64f4f95492bcd46e354b26ba
AlariCode <antonlarichev@gmail.com>
2023-07-27 17:03:56 +0300

    Упражнение - стилизация формы

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -41,12 +41,31 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			<input type='text' name='title' className={cn(styles['input'], {
-				[styles['invalid']]: !formValidState.title
-			})}/>
-			<input type='date' name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />
-			<input type='text' name='tag' />
-			<textarea name="post" id="" cols="30" rows="10" className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
+			<div>
+				<input type='text' name='title' className={cn(styles['input-title'], {
+					[styles['invalid']]: !formValidState.title
+				})}/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="date" className={styles['form-label']}>
+					<img src='/calendar.svg' alt='Иконка календаря'/>
+					<span>Дата</span>
+				</label>
+				<input type='date' name='date' id="date" className={cn(styles['input'], {
+					[styles['invalid']]: !formValidState.date
+				})} />
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="tag" className={styles['form-label']}>
+					<img src='/folder.svg' alt='Иконка папки'/>
+					<span>Метки</span>
+				</label>
+				<input type='text' id="tag" name='tag' className={styles['input']} />
+			</div>
+			
+			<textarea name="post" id="" cols="30" rows="10" className={cn(styles['input'], {
+				[styles['invalid']]: !formValidState.post
+			})}></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
