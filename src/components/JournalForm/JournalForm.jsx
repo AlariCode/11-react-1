0ed4470de7f04034e9f098ebc354fbca324f0aaf
AlariCode <antonlarichev@gmail.com>
2023-07-27 16:25:44 +0300

    Библиотека classnames

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,7 @@
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import { useState } from 'react';
+import cn from 'classnames';
 
 function JournalForm({ onSubmit }) {
 	const [formValidState, setFormValidState] = useState({
@@ -40,7 +41,9 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			<input type='text' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}/>
+			<input type='text' name='title' className={cn(styles['input'], {
+				[styles['invalid']]: !formValidState.title
+			})}/>
 			<input type='date' name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />
 			<input type='text' name='tag' />
 			<textarea name="post" id="" cols="30" rows="10" className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
