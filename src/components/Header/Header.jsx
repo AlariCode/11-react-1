81c87f0d4853678de5dc70a03d3f3cd12fecc7ca
AlariCode <antonlarichev@gmail.com>
2023-07-27 10:52:28 +0300

    CSS модули

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.css';
+import styles from './Header.module.css';
 
 function Header() {
 	return (
-		<img className='logo' src="/logo.svg" alt="Логотип журнала" />
+		<img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
 	);
 }
 
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,4 +1,4 @@
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import { useState } from 'react';
 
@@ -39,11 +39,11 @@ function JournalForm({ onSubmit }) {
 	};
 
 	return (
-		<form className='journal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title' className={`input ${formValidState.title ? '' : 'invalid'}`}/>
-			<input type='date' name='date' className={`input ${formValidState.date ? '' : 'invalid'}`} />
+		<form className={styles['journal-form']} onSubmit={addJournalItem}>
+			<input type='text' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}/>
+			<input type='date' name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />
 			<input type='text' name='tag' />
-			<textarea name="post" id="" cols="30" rows="10" className={`input ${formValidState.post ? '' : 'invalid'}`}></textarea>
+			<textarea name="post" id="" cols="30" rows="10" className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
 			<Button text="Сохранить" />
 		</form>
 	);
