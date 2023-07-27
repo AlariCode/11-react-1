d9ccd5f78d019bf5c154b356662afe542290eb73
AlariCode <antonlarichev@gmail.com>
2023-07-27 17:17:36 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
@@ -7,23 +7,19 @@ import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
-const INITIAL_DATA = [
-	// {
-	// 	id: 1,
-	// 	title: 'Подготовка к обновлению курсов',
-	// 	text: 'Горные походы открывают удивительные природные ландшафт',
-	// 	date: new Date()
-	// },
-	// {
-	// 	id: 2,
-	// 	title: 'Поход в годы',
-	// 	text: 'Думал, что очень много времени',
-	// 	date: new Date()
-	// }
-];
-
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data) {
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
