f76db2b93cd8065795bbf565851b43580467b01a
AlariCode <antonlarichev@gmail.com>
2023-08-08 15:37:01 +0300

    useMemo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import Button from '../Button/Button';
 import SelectUser from '../SelectUser/SelectUser';
 import Logo from '../Logo/Logo';
@@ -7,13 +7,9 @@ const logos = ['/logo.svg', '/vite.svg'];
 
 function Header() {
 	const [logoIndex, setLogoIndex] = useState(0);
-	const [secondIndex, setSecondIndex] = useState(0);
-	console.log('Header');
 
 	const toggleLogo = () => {
 		setLogoIndex(state => Number(!state));
-		setSecondIndex(i => i + 1);
-		console.log(secondIndex);
 	};
 
 	return (
--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,15 +1,11 @@
 import './JournalList.css';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { UserContext } from '../../context/user.context';
 
 function JournalList({ items }) {
 	const { userId } = useContext(UserContext);
-
-	if (items.length === 0) {
-		return <p>Записей пока нет, добавьте первую</p>;
-	}
 	const sortItems = (a, b) => {
 		if (a.date < b.date) {
 			return 1;
@@ -17,11 +13,18 @@ function JournalList({ items }) {
 			return -1;
 		}
 	};
+	
+	const filteredItems = useMemo(() => items
+		.filter(el => el.userId === userId)
+		.sort(sortItems), [items, userId]);
+
+	if (items.length === 0) {
+		return <p>Записей пока нет, добавьте первую</p>;
+	}
+	
 
 	return	<>
-		{items
-			.filter(el => el.userId === userId)
-			.sort(sortItems)
+		{filteredItems
 			.map(el => (
 				<CardButton key={el.id}>
 					<JournalItem 
